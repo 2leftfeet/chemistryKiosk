8adc588e521e5430806f8f9ccdc224e3018efa40
asnemokusokti <lg9666@gmail.com>
2019-08-02 18:58:24 +0300

    Setting Up Ingredient/Recipe system

---
+++ ProjectFolder/Assets/Liutauras/Scripts/Ingredient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum State{
+    Solid,
+    Liquid,
+    Gas
+}
+
+public enum Concentration{
+    High,
+    Medium,
+    Low,
+    None
+}
+
+[CreateAssetMenu]
+public class Ingredient : ScriptableObject
+{
+    public string name;
+    public Color color;
+    public Concentration concentration;
+    public State state;
+
+}
---
+++ ProjectFolder/Assets/Liutauras/Scripts/Recipe.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Recipe : ScriptableObject
+{
+    [SerializeField]
+    List<Ingredient> inputIngredients;
+
+    [SerializeField]
+    List<Ingredient> outputIngredients;
+
+}
