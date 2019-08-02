fb3cc37a8e35d2f37815c8e871528eee155ab92b
Rokevicius <rokas@kazlauskas.eu>
2019-08-02 22:56:23 +0300

    Order Pop Up functionality
    Added order pop up functionality

---
+++ ProjectFolder/Assets/Rokas/Scripts/FormulaHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FormulaHandler
+{
+    public static string Convert(string formula)
+    {
+        string result = formula;
+
+        result = result.Replace("_", "<sub>");
+        result = result.Replace("-", "</sub>");
+        
+        return result;
+    }
+}
---
+++ ProjectFolder/Assets/Rokas/Scripts/TestFormulahandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TestFormulahandler : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<TMP_Text>().text = FormulaHandler.Convert("(NH_2-)_2-CO");
+    }
+}
